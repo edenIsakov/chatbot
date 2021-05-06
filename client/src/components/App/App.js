c9752144a2d096040ea8ad3526b2de8c7e7fc87a
edenIsakov <31316799+edenIsakov@users.noreply.github.com>
2021-05-07 02:49:08 +0300

    create-react-app

---
+++ client/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatWrapper from '../ChatWrapper';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ChatWrapper />
+    </div>
+  );
+}
+
+export default App;
